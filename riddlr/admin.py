ac09d188b4595706afdbcd60bcf0af42bccf232e
rozab <roryabarnett@gmail.com>
2018-03-08 14:26:57 +0000

    configured admin interface and ManyToMany between UserProfile and Riddle

--- riddlr/admin.py
+++ riddlr/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from riddlr.models import Riddle
+
+admin.site.register(Riddle)
--- riddlr/models.py
+++ riddlr/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, models.CASCADE)
+
+    date_joined = models.DateField()
+    score = models.IntegerField()
+    karma = models.IntegerField()
+    guess_ratio = models.FloatField()
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Riddle(models.Model):
     riddle_id = models.IntegerField(unique=True)
     question = models.CharField(max_length=150)
@@ -11,24 +25,21 @@ class Riddle(models.Model):
     rating = models.IntegerField()
     num_answers = models.IntegerField()
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, models.CASCADE)
 
-
+    answered_by = models.ManyToManyField(UserProfile, through='UserAnswer')
 
     class Meta:
         verbose_name_plural = 'riddles'
-    
+
     def __str__(self):
         return self.question
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    date_joined = models.DateField()
-    score = models.IntegerField()
-    karma = models.IntegerField()
-    guess_ratio = models.FloatField()
-    picture = models.ImageField(upload_to='profile_images', blank=True)
+class UserAnswer(models.Model):
+    user = models.ForeignKey(UserProfile, models.CASCADE)
+    riddle = models.ForeignKey(Riddle, models.CASCADE)
 
-    def __str__(self):
-        return self.user.username
+    num_tries = models.IntegerField()
+    correct = models.BooleanField()
+    previous_answer = models.CharField(max_length=30)
