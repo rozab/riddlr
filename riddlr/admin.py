cd46e0266c20f83d9629a4e993a27a75d45bc5ac
rozab <roryabarnett@gmail.com>
2018-03-08 17:31:47 +0000

    trying to make templates work with bootstrap and template tags

--- riddlr/admin.py
+++ riddlr/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from riddlr.models import Riddle
+from riddlr.models import Riddle, UserProfile, UserAnswer
 from riddlr.models import UserProfile
 
 admin.site.register(Riddle)
 admin.site.register(UserProfile)
+admin.site.register(UserAnswer)
