e5a773c47d6b07f374417748449a47e4a30bc1da
rozab <roryabarnett@gmail.com>
2018-03-09 16:46:30 +0000

    removed template tags

--- riddlr/templatetags/riddlr_template_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from riddlr.models import Riddle
-
-register = template.Library()
-
-# TODO wtf is this
-@register.inclusion_tag('riddlr/riddles.html')
-def get_top_riddles():
-    return {'riddles': Riddle.objects.all().order_by('-rating')}
