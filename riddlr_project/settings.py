5ecc1744c93677d0ffa392700021dd22c272c749
rozab <roryabarnett@gmail.com>
2018-03-23 13:39:07 +0000

    changed again

--- riddlr_project/settings.py
+++ riddlr_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '_cfyp1g9+xj3dsl=fcnrory$pepb4@6=q)hmj5(n2)u@h&_#2!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://HerbertMarc.pythonanywhere.com']
 
 
 # Application definition
