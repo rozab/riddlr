c33e68d3fd17cde8eeb7b6fe1f834f2d87371ca2
Marc Herbert <macomatico@hotmail.co.uk>
2018-03-28 16:15:48 +0100

    still seems to have problems
    type errror

--- riddlr/tests.py
+++ riddlr/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from riddlr.models import Riddle, UserProfile
+from django.contrib.auth.models import User
 # Create your tests here.
 
 
@@ -7,12 +8,15 @@ from riddlr.models import Riddle, UserProfile
 
 class CreateRiddle(TestCase):
 	def test_for_user_creation(self):
-		guy = UserProfile(user=('username','james@james.com','jsjsjs'))
-		guy.save()
-		self.assertEqual((guy.karma==0), true)
+		u=User.objects.get_or_create(username="fred")
+		Up = UserProfile.objects.get_or_create(user=u)
+		Up.save()
+		self.assertEqual((Up.karma==0), true)
 
 	def test_to_ensure_rating_is_zero(self):
-		guy = UserProfile(user='james')
-		riddle = Riddle(question='How many are you', answers='two',author=guy)
+		u=User.objects.get_or_create(username="fred")
+		Up = UserProfile.objects.get_or_create(user=u)
+		Up.save()
+		riddle = Riddle(question='How many are you', answers='two',author=Up)
 		riddle.save()
 		self.assertEqual((riddle.rating == 0), true)
